b8c4a8bdd77db1a25eaa3bed71fb218c4e075c23
Yunru <eddyecat@gmail.com>
2026-01-20 09:47:21 +0800

    - 修改音效

--- Assets/Scripts/Gameplay/Player/SkillExecutor.cs
+++ Assets/Scripts/Gameplay/Player/SkillExecutor.cs
@@ -84,7 +84,7 @@ namespace Tenronis.Gameplay.Player
             }
             else
             {
-                GameEvents.TriggerPlayImpactSound();
+                // GameEvents.TriggerPlayImpactSound();
             }
         }
         
